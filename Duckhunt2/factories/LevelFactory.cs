6e8f4281cb6d4c2e47a630cac34f57f27a49efec
Ivan Horn <ivanhorn1990@gmail.com>
2015-10-08 06:50:36 +0200

    Deze versie draait....

--- Duckhunt2/factories/LevelFactory.cs
+++ Duckhunt2/factories/LevelFactory.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Duckhunt2.Levels;
 
 namespace Duckhunt2.factories {
     class LevelFactory {
         public static Level create(String id) {
-
+            throw new NotImplementedException();
         }
     }
 }
